f9a613247af175736f462945af71ca69df8638f3
iyuowert <161446069+iyuowert@users.noreply.github.com>
2024-04-16 10:56:28 +0200

    Update Skrypt2.js

---
+++ JavaScript/Skrypt2.js
@@ -0,0 +1,25 @@
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-2">
+<SCRIPT LANGUAGE= "JavaScript" type= "text/javascript">
+<!-- Ukrycie przed przeglądarkami nie obsługującymi JavaScriptów
+function printBanner()
+{
+  data = new Date();
+  num = data.getDay();
+ 
+  name = "banner" + num + ".gif";
+  tekst = "<IMG SRC='" + name + "'>";
+  document.write(tekst);
+}
+// Koniec kodu JavaScript -->
+</SCRIPT>
+</HEAD>
+<BODY>
+<SCRIPT LANGUAGE= "JavaScript" type= "text/javascript">
+<!-- Ukrycie przed przeglądarkami nie obsługującymi JavaScriptów
+printBanner();
+// Koniec kodu JavaScript -->
+</SCRIPT>
+</BODY>
+</HTML>
